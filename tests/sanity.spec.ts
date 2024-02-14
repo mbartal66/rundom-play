88e791edf62248060031964fff2c94feaa8dccf2
Meir Bar-Tal <meir.bartal@gmail.com>
2024-02-14 22:05:39 +0200

    Removed obsolete lines of code

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -5,12 +5,10 @@ test('test', async ({ page }) => {
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
-
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
   await page.locator('a').filter({ hasText: '3' }).click();
-  
   await page.locator('[data-test="checkout"]').click();
   await page.locator('[data-test="firstName"]').fill('Meir');
   await page.locator('[data-test="lastName"]').fill('Bar-Tal');
